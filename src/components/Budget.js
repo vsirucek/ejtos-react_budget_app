685d022e4358b6286e74438ef50e5f3e9bcc90ef
vsirucek <v.sirucek@icloud.com>
2022-12-28 09:32:39 -0500

    Changes in the Budget.js by Vlastimil Sirucek

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, expenses } = useContext(AppContext);
+    const { action, setAction} = useState('');
+
+    const setBudget = (value) =>{
+        if ( value>20000) {
+            alert("The budget may not exceed £20.000");
+            return;
+        }
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+
+        if ( value < totalExpenses){
+            alert("You cannot reduce the budget value lower than the spending");
+            return;            
+        }
+        
+        dispatch({  type:   'SET_BUDGET',
+                    payload: value,
+        });
+    };    
 
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+            <input
+                        required='required'
+                        type='number'
+                        id='budget'
+                        step='10'
+                        value={budget}
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => setBudget(event.target.value)
+                        }>
+            </input>
+        </span>
         </div>
     );
 };
